c815ee08bd35d6dea8ed3ba8263ff141f6a4d218
Alpesh Sawal <sawalalpesh532@gmail.com>
2025-01-05 23:37:09 +0530

    made UI changes in details

--- src/components/MoreDetails/MoreDetails.js
+++ src/components/MoreDetails/MoreDetails.js
@@ -11,19 +11,23 @@ function MoreDetails({ closeMoreDetails }) {
     hideDetails();
   }
 
+  function titleCase(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
   if (!isDetailsVisible || !detailsData) return null;
   return (
     <div className="more-details">
       <img src={detailsData.sprites.front_default} alt="" />
-      <p className="poke-name">{detailsData.name}</p>
+      <p className="poke-name">{titleCase(detailsData.name)}</p>
       <div className="details">
         <div className="types">
           <h3>Types</h3>
-          <div className="item-types">{detailsData.types ? detailsData.types.map((item) => <p key={item.slot}>{item.type.name}</p>) : null}</div>
+          <div className="item-types">{detailsData.types ? detailsData.types.map((item) => <p key={item.slot}>{titleCase(item.type.name)}</p>) : null}</div>
         </div>
         <div className="abilities">
           <h3>Abilities</h3>
-          <div className="item-abilities">{detailsData.abilities ? detailsData.abilities.map((item, index) => <p key={index}>{item.ability.name}</p>) : null}</div>
+          <div className="item-abilities">{detailsData.abilities ? detailsData.abilities.map((item, index) => <p key={index}>{titleCase(item.ability.name)}</p>) : null}</div>
         </div>
         <div className="stats">
           <h3>Stats</h3>
@@ -31,7 +35,7 @@ function MoreDetails({ closeMoreDetails }) {
             {detailsData.stats
               ? detailsData.stats.map((item, index) => (
                   <p key={index}>
-                    <span>{item.stat.name}</span>: {item.base_stat}
+                    <span>{titleCase(item.stat.name)}</span> : {item.base_stat}
                   </p>
                 ))
               : null}
